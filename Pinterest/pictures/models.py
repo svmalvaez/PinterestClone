42b332e65612c3444c5f012f05e0254fe97ff524
svmalvaez <svmalvaez@gmail.com>
2015-04-28 16:33:50 -0500

    Views and Templates

--- Pinterest/pictures/models.py
+++ Pinterest/pictures/models.py
@@ -1,14 +1,14 @@
 from django.db import models
-from django.auth.contrib.models import User
+from django.contrib.auth.models import User
 # Create your models here.
 class Picture(models.Model):
 	name = models.CharField(max_length=30, null=True, blank=True)
-	description = models.TextField(max_length=50 null=True, blank=True)
-	photo = models.TextField(max_length=50 null=True, blank=True)
-	like = models.BooleanField(default=False null=True, blank=True)
+	description = models.TextField(max_length=50, null=True, blank=True)
+	photo = models.ImageField(upload_to='static/img/')
+	like = models.BooleanField(default=False)
 	user = models.ForeignKey(User)
 
 class Tablero(models.Model):
-	name = models.CharField(max_length=30 null=True, blank=True)
+	name = models.CharField(max_length=30, null=True, blank=True)
 	picture = models.ForeignKey(Picture)
 	user = models.ForeignKey(User)
--- Pinterest/pictures/views.py
+++ Pinterest/pictures/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views.generic.base import View
 
-# Create your views here.
+class UploadPin(View):
+
+	def get(self, request):
+		return render(request, 'upload.html')
+
+	def post(self, request):
+		return render(request, 'upload.html')
+
+class Pin(View):
+
+	def get(self, request):
+		return render(request, 'pin_index.html')
